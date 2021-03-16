076ea45679209c8bda6cc6309afd835600dda743
Niels <info@heimseiten.de>
2021-03-16 22:11:28 +0100

    added select_toggler

--- src/Resources/public/universal-toggler.js
+++ src/Resources/public/universal-toggler.js
@@ -1,100 +1,133 @@
-document.addEventListener("DOMContentLoaded", function(event) {   
-	start();
-	
-	function start() {
-		document.querySelector('body').classList.add('toggler_active')
-		activateClickToggler('.click_toggler')
-		activateViewToggler('.view_toggler')
-	}
+start()
 
-	function activateViewToggler(togglerClass) {
-		document.querySelectorAll(togglerClass+':not(.widget)').forEach(toggler => {
-			setInitialStatus(toggler, togglerClass)
-			changeOnView(toggler, togglerClass)
-		})
-	}
+function start() {
+	document.querySelector('body').classList.add('toggler_active')
+	activateClickToggler('.click_toggler')
+	activateViewToggler('.view_toggler')
+	activateSelectToggler('.select_toggler')
+}
 
-	function changeOnView(toggler, togglerClass) { 
-		for ( q=0; q<elementsToToggle(toggler).length;q++ ) {
-			var cssPath = '.'+elementsToToggle(toggler)[q]+ ':not(' + togglerClass + '):not(.'+elementsToToggle(toggler)[q]+ ' > .'+elementsToToggle(toggler)[q]+')'
-		} 
-		inViewport(toggler, callbackElement => {
-			if (callbackElement.isIntersecting) { 
-				switchToActive(true, cssPath)
-			 } else { 
-				switchToActive(false, cssPath)
-			 }
-		})
-	}
-
-	function inViewport(elem, callback) {
-		return new IntersectionObserver(entries => {
-			entries.forEach(entry => callback(entry))
-		}).observe(elem)
-	  }
-
-	function activateClickToggler(togglerClass) {
-		document.querySelectorAll(togglerClass+':not(.widget)').forEach(toggler => {
-			setInitialStatus(toggler, togglerClass)
-			changeOnClick(toggler, togglerClass)
-		})
-	}
-
-	function changeOnClick(toggler, togglerClass) {  
+function activateClickToggler(togglerClass) {
+	document.querySelectorAll(togglerClass+':not(.widget)').forEach(toggler => {
+		setInitialStatus(toggler, togglerClass)
 		toggler.addEventListener('mouseup',e => {
-			if ( toggler.querySelector('input.checkbox') ) {
-				if (e.path[0].tagName!='LABEL' && e.path[0].tagName!='INPUT') { 
-					if (toggler.querySelector('input.checkbox').checked) { 
-						toggler.querySelector('input.checkbox').checked = false 
-					} else { toggler.querySelector('input.checkbox').checked = true }  
+			changeCheckboxStatus(toggler)
+			for ( let i=0; i<toggleElements(toggler).length; i++ ) {
+				const elementSelector = builtElementSelector(togglerClass, toggleElements(toggler)[i])
+				if ( document.querySelector(elementSelector) ) {
+					var condition = !document.querySelector(elementSelector).classList.contains('t_active')
+				}
+				setActiveState(condition, elementSelector)
+			}
+		})	
+	})
+}
+
+function activateViewToggler(togglerClass) {
+	document.querySelectorAll(togglerClass+':not(.widget)').forEach(toggler => {
+		setInitialStatus(toggler, togglerClass)
+		for ( let i=0; i<toggleElements(toggler).length; i++ ) {
+			const elementSelector = builtElementSelector(togglerClass, toggleElements(toggler)[i])
+			inViewport(toggler, callbackElement => {
+				if (callbackElement.isIntersecting) { 
+					setActiveState(true, elementSelector)
+				 } else { 
+					setActiveState(false, elementSelector)
+				 }
+			})		
+		}	
+	})
+}
+
+function inViewport(elem, callback) {
+	return new IntersectionObserver(entries => {
+		entries.forEach(entry => callback(entry))
+	}).observe(elem)
+}
+
+function activateSelectToggler(togglerClass) {
+	document.querySelectorAll(togglerClass+':not(.widget)').forEach(toggler => {
+		toggler.classList.add('toggle_'+ toggler.value)
+		setInitialStatus(toggler, togglerClass)
+		toggler.addEventListener('change',e => {			
+			for ( let i=0; i<toggleElements(toggler).length; i++ ) {
+				const elementSelector = builtElementSelector(togglerClass, toggleElements(toggler)[i])
+				if ( document.querySelector(elementSelector) ) {
+					setActiveState(false, elementSelector)
+				}	
+			}
+	
+			for ( let i=0; i<e.target.classList.length; i++) {
+				if (e.target.classList[i].includes('toggle_')) {
+					e.target.classList.remove(e.target.classList[i])
 				}
 			}
-			for ( q=0; q<elementsToToggle(toggler).length;q++ ) {
-				var cssPath = '.'+elementsToToggle(toggler)[q]+ ':not(' + togglerClass + '):not(.'+elementsToToggle(toggler)[q]+ ' > .'+elementsToToggle(toggler)[q]+')'
-				if ( document.querySelector(cssPath) ) {
-					var condition = !document.querySelector(cssPath).classList.contains('t_active')
-				}
-				switchToActive(condition, cssPath)
+	
+			toggler.classList.add('toggle_'+ toggler.value)
+	
+			for ( let i=0; i<toggleElements(toggler).length; i++ ) {
+				const elementSelector = builtElementSelector(togglerClass, toggleElements(toggler)[i])
+				if ( document.querySelector(elementSelector) ) {
+					setActiveState(true, elementSelector)
+				}	
+			}
+	
+		})
+	})
+}
+
+function setInitialStatus(toggler, togglerClass) {
+	for ( let t=0; t<toggleElements(toggler).length; t++ ) {
+		const elementSelector = builtElementSelector(togglerClass, toggleElements(toggler)[t])
+		if ( toggler.querySelector('input.checkbox') ) {
+			setActiveState(toggler.querySelector('input.checkbox').checked, elementSelector)
+		} else {
+			setActiveState(true, elementSelector)
+		}
+	}
+}
+
+function toggleElements(element) {  
+	var toggleElements = []
+	for (i=0; i<element.classList.length;i++) {
+		if (element.classList[i].includes('toggle_')) {
+			toggleElements.push(element.classList[i])
+		}
+	}
+	return toggleElements;
+}
+
+function setActiveState(condition,selector) {
+	if ( document.querySelector(selector) ) {
+		document.querySelectorAll(selector).forEach( e => {
+			switch (condition) {
+				case true:  		
+					e.classList.add('t_active')
+					e.classList.remove('t_inactive') 
+					break
+				case false:
+					e.classList.remove('t_active')
+					e.classList.add('t_inactive') 
+					break
+				default:
+					break
 			}
 		})
 	}
+}
 
-	function setInitialStatus(toggler, togglerClass) {  
-		for ( t=0; t<elementsToToggle(toggler).length;t++ ) {
-			var cssPath = '.'+elementsToToggle(toggler)[t]+ ':not(' + togglerClass + '):not(.'+elementsToToggle(toggler)[t]+ ' > .'+elementsToToggle(toggler)[t]+')'
-			if ( toggler.querySelector('input.checkbox') ) {
-				switchToActive(toggler.querySelector('input.checkbox').checked,cssPath)
-			} else {
-				switchToActive(true,cssPath)
-			}
+function changeCheckboxStatus(toggler) {
+	if ( toggler.querySelector('input.checkbox') ) {
+		if (e.path[0].tagName!='LABEL' && e.path[0].tagName!='INPUT') { 
+			if (toggler.querySelector('input.checkbox').checked) { 
+				toggler.querySelector('input.checkbox').checked = false 
+			} else { toggler.querySelector('input.checkbox').checked = true }  
 		}
 	}
-	
-	function elementsToToggle(element) {  
-		var toggleElements = []
-		for (i=0; i<element.classList.length;i++) {
-			if (element.classList[i].includes('toggle_')) {
-				toggleElements.push(element.classList[i])
-			}
-		}
-		return toggleElements;
-	  }
+}
 
-	function switchToActive(condition,selector) {
-		if ( document.querySelector(selector) ) {
-			if (condition) { 
-				document.querySelectorAll(selector).forEach(te => {	
-					te.classList.add('t_active')
-					te.classList.remove('t_inactive') 
-				})
-			} 
-			else { 
-				document.querySelectorAll(selector).forEach(te => {	
-					te.classList.remove('t_active')
-					te.classList.add('t_inactive') 
-				})
-			}
-		}
-	}
-
-});
+function builtElementSelector(togglerClass, toggleElementsClass) {
+	// return '.toggle_123:not(.click_Toggler):not( .toggle_123 > .toggle_123 )
+	   return '.' + toggleElementsClass + ':not(' + togglerClass + '):not(.' + toggleElementsClass + ' > .' + toggleElementsClass + ')'
+   }
+   
