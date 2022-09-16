491648d9d8037aae47e667ef2a7716af48fb11ac
Niels <info@heimseiten.de>
2022-09-16 16:45:54 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,6 +1,4 @@
 <?php
 
-if (TL_MODE == 'FE') {
-    $GLOBALS['TL_HEAD'][] = '<script defer src="/bundles/heimseitencontaouniversaltoggler/universal-toggler.js"></script>';
-    $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaouniversaltoggler/universal-toggler.scss|static';
-}
+$GLOBALS['TL_HEAD'][] = '<script defer src="/bundles/heimseitencontaouniversaltoggler/universal-toggler.js"></script>';
+$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaouniversaltoggler/universal-toggler.scss|static';
