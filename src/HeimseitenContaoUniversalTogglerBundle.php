9e03ced22723a8f23f304fa53d66cbfead8a3cf2
Niels <info@heimseiten.de>
2026-06-23 10:32:31 +0200

    Modernize bundle layout (src/Resources → project root, AbstractBundle)

--- src/HeimseitenContaoUniversalTogglerBundle.php
+++ src/HeimseitenContaoUniversalTogglerBundle.php
@@ -2,23 +2,10 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Universal Toggler.
- * 
- * (c) heimseiten.de - Webdesign aus Köln 2021 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-universal-toggler-bundle
- */
-
 namespace Heimseiten\ContaoUniversalTogglerBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-/**
- * Class HeimseitenContaoUniversalTogglerBundle
- */
-class HeimseitenContaoUniversalTogglerBundle extends Bundle
+class HeimseitenContaoUniversalTogglerBundle extends AbstractBundle
 {
 }
